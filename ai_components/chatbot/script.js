cf96f1961098177197c51628f0d3d88eed025463
MunmunPatra <munmunpatra162@gmail.com>
2024-07-14 19:49:45 +0530

    Add/Update code in ai_components/chatbot

---
+++ ai_components/chatbot/script.js
@@ -0,0 +1,83 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const chatbotCloseBtn = document.querySelector(".close-btn");
+
+let userMessage;
+
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+    
+    const ChatLi = document.createElement("li");
+    ChatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    ChatLi.innerHTML = chatContent;
+    ChatLi.querySelector("p").textContent = message;
+    return ChatLi;
+}
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL = " https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDym_mJeND2Vn64B9q5QaN16_9ibbUiUYA";
+    const messageElement = incomingChatLi.querySelector("p");
+    
+    const requestOptions = {
+        method:"POST",
+        headers:{
+        "Content-Type": "application/json"
+        
+        },
+        body: JSON.stringify({
+            contents:[{parts:[{text:userMessage}]}]
+        
+        })
+    }
+
+
+    fetch(API_URL,requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.candidates[0].content.parts[0].text;
+    }).catch((error) => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+    
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+
+        const incomingChatLi = createChatLi("Thinking...", "incoming")
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    },600);
+}
+
+
+chatInput.addEventListener("input", () => {
+    
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    
+
+    if(e.key === "Enter" && !e.shiftkey && window.innerWidth > 800){
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
