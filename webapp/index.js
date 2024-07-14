4f8699140e4a36a9edbaeeedf35f520c809ea4e4
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-07-14 21:30:13 +0530

    myntra homepage for navigating to fashion spy, fashion spy page, server side apis, blog generator, gemini api

--- webapp/index.js
+++ webapp/index.js
@@ -1,39 +1,28 @@
 const express = require("express");
 const app = express();
-
-//making view directory available at the backend directory
 const path = require("path");
-
 let port = 8080;
-
-//setting template i.e., view engine to ejs
 app.set("view engine", "ejs");
-
-//making view  available at the backend not only for EJS directory
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public/js")));
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/prerequisites")));
 
-//subtemplating
-app.use(express.static(path.join(__dirname, "public/js")));
-app.use(express.static(path.join(__dirname, "public/css")))
-
-
+//blog-generator
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+const { type } = require("os");
 const genAI = new GoogleGenerativeAI("AIzaSyCb0VYilaCXsj0XEcsw6fkBLHsGckTm8sI");
-
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
 let topics = [
 	"trending colors for this season?",
 	"trending prints for this season?",
 	"trending patterns for this season?",
 	"Fashion Trends for Different Body Types",
-	"Top 10 Fashion trends for 2024",
+	"Top 10 Fashion trends for 2025",
 	"Workwear Evolution: Professional Fashion Trends for the Modern Office",
 	"The Intersection of Fashion and Technology: Wearable Tech Trends",
 	"The Rise of Sustainable Fashion: Eco-Friendly Trends to Follow",
 ];
-
 let audience = [
 	"fashion enthusiasts",
 	"genZ",
@@ -43,36 +32,44 @@ let audience = [
 	"earning genZ",
 ];
 
-let blog_topic = topics[Math.floor(Math.random()*topics.length)];
-let blog_audience = audience[Math.floor(Math.random()*audience.length)];
-
-async function run() {
-	const prompt = `Write a fashion blog on the topic ${blog_topic} for ${blog_audience}`;
-  
-	const result = await model.generateContent(prompt);
-	const response = await result.response;
-	const text = response.text();
-	console.log(text);
-  }
-  
-let content = run();
-
-
 //listening
 app.listen(port, () => {
     console.log(`port is listening at port number ${port}`);
 });
 
+//index route
+app.get("/", (req, res)=>{
+	res.render("home.ejs");
+});
+
 //view route
-app.get("/", (req,res) => {
-	let customers = ["Priya", "Ritika", "Tisha", "Anjali", "Eshita", "Munmun"];
-	randIdx = Math.floor(Math.random()*customers.length)
-	let name = customers[randIdx];
-	// For the time being, consider this data to be coming from the database.
+app.get("/:username/fashionspy", async(req, res) => {
+	let {username} = req.params;
+	username = username[0].toUpperCase()+username.slice(1);
 
-	// let { name } = req.params;
+	let blog_topic = topics[Math.floor(Math.random()*topics.length)];
+	let blog_audience = audience[Math.floor(Math.random()*audience.length)];
 
-	res.render("blog.ejs", {customerName:name, blogContent: content});
-	console.log(content);
-	console.log("home page rendered successfully.");
+	async function run() {
+		const prompt = `Write a fashion blog on the topic ${blog_topic} for ${blog_audience}, remember to start the blog body by stating 'Let's start.'`;
+	  
+		const result = await model.generateContent(prompt);
+		const response = await result.response;
+		const text = response.text();
+		return text;
+	}
+
+	let content = await run();
+	console.log(typeof(content));
+
+	//post-processing- removing unnecessary characters
+	let contentStr = await content.toString();
+	contentStr = await contentStr.replaceAll("*", "");
+	contentStr = await contentStr.replaceAll("#", "");
+	console.log(contentStr);
+	
+	let title = await contentStr.slice(0, contentStr.indexOf(":"));
+	// console.log(body)
+
+	res.render("blog.ejs", {customerName : username, blogTitle: title, blogContent : contentStr});	
 });
